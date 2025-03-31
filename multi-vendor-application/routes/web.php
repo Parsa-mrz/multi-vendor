ebf199609c054ba1260312a8f6ab3835de4266db
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2025-04-01 03:08:02 +0330

    fix: :bug: fix smtp

--- multi-vendor-application/routes/web.php
+++ multi-vendor-application/routes/web.php
@@ -4,14 +4,3 @@ use App\Livewire\HomePage;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', HomePage::class)->name('home');
-Route::get('test-mail', function () {
-    $content = '<h1>Hello!</h1><p>This is a test email from Laravel.</p><p>✅ SMTP is working!</p>';
-
-    Mail::send([], [], function ($message) use ($content) {
-        $message->to('recipient@example.com')
-              ->subject('Test Email')
-              ->setBody($content, 'text/html');
-    });
-
-    return 'Email Sent!';
-});
