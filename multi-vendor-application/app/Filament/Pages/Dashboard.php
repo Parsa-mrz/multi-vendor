ede87afaace327d9bdea20bdf4b9150aa670c9b5
Parsa Mirzaie <Mirzaie_parsa@protonmail.ch>
2025-04-12 13:15:10 +0330

    :sparkles: add pulse to admin dashboard

--- multi-vendor-application/app/Filament/Pages/Dashboard.php
+++ multi-vendor-application/app/Filament/Pages/Dashboard.php
@@ -3,18 +3,39 @@
 namespace App\Filament\Pages;
 
 use App\Livewire\Dashboard\Main\CustomerStats;
+use Dotswan\FilamentLaravelPulse\Widgets\PulseCache;
+use Dotswan\FilamentLaravelPulse\Widgets\PulseExceptions;
+use Dotswan\FilamentLaravelPulse\Widgets\PulseQueues;
+use Dotswan\FilamentLaravelPulse\Widgets\PulseServers;
+use Dotswan\FilamentLaravelPulse\Widgets\PulseSlowOutGoingRequests;
+use Dotswan\FilamentLaravelPulse\Widgets\PulseSlowQueries;
+use Dotswan\FilamentLaravelPulse\Widgets\PulseSlowRequests;
+use Dotswan\FilamentLaravelPulse\Widgets\PulseUsage;
 use Filament\Pages\Dashboard as BaseDashboard;
 use Illuminate\Support\Facades\Auth;
 
+
 class Dashboard extends BaseDashboard
 {
+
+    public function getColumns(): int|string|array
+    {
+        return 12;
+    }
     public function getWidgets(): array
     {
         $user = Auth::user();
 
         if ($user->isAdmin()) {
             return [
-                CustomerStats::class,
+                PulseServers::class,
+                PulseCache::class,
+                PulseExceptions::class,
+                PulseUsage::class,
+                PulseQueues::class,
+                PulseSlowQueries::class,
+                PulseSlowRequests::class,
+                PulseSlowOutGoingRequests::class
             ];
         } elseif ($user->isCustomer()) {
             return [
